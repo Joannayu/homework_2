8179125c27d90f9914fc1cfba522e2fab73aafb1
YU Zhou <zhouyu@thoughtworks.com>
2020-01-11 22:58:06 +0800

    add price json

---
+++ price-json.js
@@ -0,0 +1,17 @@
+let priceList = [{
+	SKU: 'ipd',
+	name: 'Super Ipad',
+	price: 549.99
+}, {
+	SKU: 'mbp',
+	name: 'Macbook Pro',
+	price: 1399.99
+}, {
+	SKU: 'atv',
+	name: 'Apple TV',
+	price: 109.50
+}, {
+	SKU: 'vga', 
+	name: 'VGA adapter',
+	price: 30.00
+}]
